41c5c557da2c73fc56cd17d2ef8f58a1f090d50d
pronajitdey <deypronajit@gmail.com>
2026-08-07 23:37:06 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 
 namespace fs = std::filesystem;
 
-bool isFileExecutable(fs::path &file_path);
+bool isFileExecutable(const fs::path &file_path);
 
 int main() {
   // Flush after every std::cout / std:cerr
