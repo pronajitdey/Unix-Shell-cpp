1b35b96accfacbf0f9a1684154e00fd5d31811cf
pronajitdey <deypronajit@gmail.com>
2026-08-07 20:55:10 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,22 +5,34 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  
+  std::string input;
+  std::string command;
+  std::string arguments;
 
   while (true) {
     // TODO: Uncomment the code below to pass the first stage
     std::cout << "$ ";
     
-    std::string command;
-    std::getline(std::cin, command);
+    std::getline(std::cin, input);
+
+    size_t first_space = input.find(' ');
+    if (first_space != std::string::npos) {
+      command = input.substr(0, first_space);
+      arguments = input.substr(first_space + 1);
+    } else {
+      command = input;
+      arguments = "";
+    }
 
     if (command == "exit") break;
-    else if (command.substr(0, 5) == "echo ") {
-      std::cout << command.substr(5) << std::endl;
-    } else if (command.substr(0, 5) == "type ") {
-      if (command.substr(5) == "echo" || command.substr(5) == "exit" || command.substr(5) == "type") {
-        std::cout << command.substr(5) << " is a shell builtin" << std::endl;
+    else if (command == "echo") {
+      std::cout << arguments << std::endl;
+    } else if (command == "type ") {
+      if (arguments == "echo" || arguments == "exit" || arguments == "type") {
+        std::cout << arguments << " is a shell builtin" << std::endl;
       } else {
-        std::cout << command.substr(5) << ": not found" << std::endl;
+        std::cout << arguments << ": not found" << std::endl;
       }
     } else {
       std::cout << command << ": command not found" << std::endl;
