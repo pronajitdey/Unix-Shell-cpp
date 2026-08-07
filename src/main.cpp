6e06863202176683f4f939d240c75b8f313e8e13
pronajitdey <deypronajit@gmail.com>
2026-08-07 20:31:55 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@ int main() {
     
     std::string command;
     std::getline(std::cin, command);
+
+    if (command.compare("exit")) break;
     
     std::cout << command << ": command not found" << std::endl;
   }
